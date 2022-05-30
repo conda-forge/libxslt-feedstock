ac7ce9eeabb16095da938f2a89f7a4f509fc083b
Filipe Fernandes <ocefpaf@gmail.com>
2022-05-30 15:46:59 -0300

    let's try Cheng's suggestion

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+sed -i.bak -e 's/-llzma //g' -e 's/-lz //g' $PREFIX/bin/xml2-config
+
+# It looks like an missing symbol of libxml2 on osx-arm64
+if [[ ${target_platform} =~ .*arm64.* ]]; then
+    LDFLAGS="${LDFLAGS} -lxml2"
+fi
+
 ./configure --prefix=$PREFIX \
             --with-libxml-prefix=$PREFIX \
             --without-python
@@ -11,6 +18,7 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}
 make check
 fi
 make install
+
 
 # We can remove this when we start using the new conda-build.
 find $PREFIX -name '*.la' -delete
